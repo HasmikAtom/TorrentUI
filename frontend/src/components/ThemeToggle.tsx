c76a60e9a37f2fb9668654e59f9da14a9d271257
Diveafall <d.isayan@gmail.com>
2026-04-25 17:39:38 +0400

    shrink ThemeToggle to default icon-button size

--- frontend/src/components/ThemeToggle.tsx
+++ frontend/src/components/ThemeToggle.tsx
@@ -15,9 +15,9 @@ export function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="h-14 w-14">
-          <Sun className="h-6 w-6 rotate-0 scale-[1.5] transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-[1.5]" />
+        <Button variant="outline" size="icon" className="relative">
+          <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
